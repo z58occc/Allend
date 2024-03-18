a00426fb698364ef962abbe74bb6233fd5fb9f86
XiaoXiaTian2 <s59444997880@gmail.com>
2024-03-18 20:47:40 +0800

    install_breeze

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CommitController;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\VideoController;
-use App\Http\Controllers\WorkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,21 +9,11 @@ use App\Http\Controllers\WorkController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/commit_crime', CommitController::class);
-
-Route::post('/service',ServiceController::class);
-
-Route::post('work',WorkController::class);
-
-Route::post('video',VideoController::class);
-
-
