b86fa31ee30c12deaf729b57a039021d6dcad946
shritesong <bird136@gmail.com>
2024-03-20 11:45:39 +0800

    IFindpeople & add updated_at now

--- backend/app/Http/Controllers/IFindCommitController.php
+++ backend/app/Http/Controllers/IFindCommitController.php
@@ -9,8 +9,43 @@ class IFindCommitController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $id = $request->id;
-        $demands = DB::table('demmand')->where('d_type',$id)->get();
+
+        $query = DB::table('demmand');
+
+        if($request->has('d_type')){
+            $query->where('d_type',$request->d_type);
+        }
+
+        if($request->has('d_duration')){
+            $query->where('d_duration',$request->d_duration);
+        }
+    
+        if($request->has('d_active_location')){
+            $query->whereIn('d_active_location',explode(',',$request->d_active_location));
+        }
+        if($request->has('d_amount')){
+            $query->whereIn('d_amount',$request->d_amount);
+        }
+        if($request->has('order')){
+            $order = $request->order;
+        }
+
+        switch($order){
+                case '1':
+                    $query->orderBy('created_at','desc');
+                case '2':
+                    $query->orderBy('updated_at','desc');
+                case '3':
+                    $query->orderBy('d_amount','desc');
+                // case '4':
+                    //接案人數
+        
+        }
+
+
+        $demands = $query->get();
         return response()->json($demands);
+
     }
+
 }
