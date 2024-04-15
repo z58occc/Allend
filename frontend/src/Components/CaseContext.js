b28922ee1824d20c0f677f75987e831e0a4a8c8d
z58occc <a0981583030@gmail.com>
2024-04-15 22:05:51 +0800

    lee update

--- frontend/src/Components/CaseContext.js
+++ frontend/src/Components/CaseContext.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { VscAccount } from "react-icons/vsc";
 import { CiStar } from "react-icons/ci";
@@ -12,7 +12,7 @@ import axios from 'axios';
 
 
 function CaseContext() {
-    const {isLoggedIn, setIsLoggedIn, handleShow} = useContext(IsLoggedInContext);
+    const { isLoggedIn, setIsLoggedIn, handleShow } = useContext(IsLoggedInContext);
 
     const close = async () => {
         setShow(false);
@@ -86,17 +86,17 @@ function CaseContext() {
     const { id } = useParams();
 
     async function fetchData(id) {
-        if (isLoggedIn){
+        if (isLoggedIn) {
             fetch(`http://localhost/Allend/backend/public/api/demmand_content/${id}`,
-            {headers:{'Authorization': `Bearer ${Cookies.get('token')}`}})
+                { headers: { 'Authorization': `Bearer ${Cookies.get('token')}` } })
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data.dammand)
                     setPosts(data.dammand);
                     setMembers(data.members);
                     setServiceStarAvg(data.service_star_avg);
-            })
-        }else{
+                })
+        } else {
             fetch(`http://localhost/Allend/backend/public/api/demmand_content/${id}`)
                 .then((response) => response.json())
                 .then((data) => {
@@ -104,36 +104,82 @@ function CaseContext() {
                     setPosts(data.dammand);
                     setMembers(data.members);
                     setServiceStarAvg(data.service_star_avg);
-            })
+                })
         }
     }
+    // 相同類別案件
+    const [othercase, setOthercase] = useState([]);
+    const [type, setType] = useState("");
+    // if (posts.d_type == "網站設計" && type != 1) {
+    //     setType(1);
+    // }
+    console.log(posts.d_type);
+    if (type == "") {
+        switch (posts.d_type) {
+            case "網站設計":
+                setType(1);
+                console.log(1);
+                break;
+            case "軟體程式":
+                setType(2);
+                break;
+            case "平面設計":
+                setType(3);
+                break;
+            case "文字語言":
+                setType(4);
+                break;
+            case "專業諮詢":
+                setType(5);
+                break;
+
+            default:
+                break;
+        }
+    }
+    const fetchDataOther = async () => {
+
+        let url = `http://localhost/Allend/backend/public/api/findcase?type=${type}`;
+        console.log(type);
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+
+                setOthercase(data);
+                console.log(data);
+            });
+
+    };
+
+
+    // 相同類別案件
 
     useEffect(() => {
-        if(Cookies.get('token')){
+        if (Cookies.get('token')) {
             setIsLoggedIn(true)
         }
-      
-        fetchData(id);
 
-    }, [id, isLoggedIn]);
+        fetchData(id);
+        fetchDataOther();
+    }, [type, id, isLoggedIn]);
 
     const [textShow, setTextShow] = useState(false);
     // 加入收藏
     const addServiceCollection = (did) => {
         axios({
-        method: "post",
-        url: "http://localhost/Allend/backend/public/api/addcollection",
-        data:{ did: did },
-        headers: { Authorization: `Bearer ${Cookies.get('token')}` }
+            method: "post",
+            url: "http://localhost/Allend/backend/public/api/addcollection",
+            data: { did: did },
+            headers: { Authorization: `Bearer ${Cookies.get('token')}` }
         })
-        .then((res) => {
-            setPosts({...posts, fid: res.data.fid.fid})
-            setTextShow(true)
-            setTimeout(()=>{
-                setTextShow(false)
-            }, 3000)
-        })
-        .catch((err) => {console.log(err)})
+            .then((res) => {
+                setPosts({ ...posts, fid: res.data.fid.fid })
+                setTextShow(true)
+                setTimeout(() => {
+                    setTextShow(false)
+                }, 3000)
+            })
+            .catch((err) => { console.log(err) })
     }
 
     // 取消收藏
@@ -144,10 +190,10 @@ function CaseContext() {
             data: { fid: fid },
             headers: { Authorization: `Bearer ${Cookies.get('token')}` }
         })
-        .then((res) => {
-            setPosts({...posts, fid: null})
-        })
-        .catch((err) => {console.log(err)})
+            .then((res) => {
+                setPosts({ ...posts, fid: null })
+            })
+            .catch((err) => { console.log(err) })
     }
 
 
@@ -156,11 +202,15 @@ function CaseContext() {
     const redTextStyle = {
         color: 'red'
     };
-console.log(service_star_avg)
+    console.log(service_star_avg)
+
+
+
+
 
     return (
         <>
-            <div className='container'>
+            <div className='container vh-100'>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -187,12 +237,12 @@ console.log(service_star_avg)
                             </ul>
                         </div>
                         <div className='mt-5'>
-                        {isLoggedIn === true && posts.fid
-                        ?   <>
-                                <FaHeart size={25} style={{ color: 'red', cursor: 'pointer', marginRight: '5px'}} onClick={() => {cancelServiceCollection(posts.fid)}}/>
-                                {textShow && <span style={{color: 'red'}}>已收藏！</span>}
-                            </>
-                        :   <FaRegHeart size={25} style={{cursor: 'pointer'}} onClick={isLoggedIn ? () => {addServiceCollection(posts.did)} : handleShow} />}
+                            {isLoggedIn === true && posts.fid
+                                ? <>
+                                    <FaHeart size={25} style={{ color: 'red', cursor: 'pointer', marginRight: '5px' }} onClick={() => { cancelServiceCollection(posts.fid) }} />
+                                    {textShow && <span style={{ color: 'red' }}>已收藏！</span>}
+                                </>
+                                : <FaRegHeart size={25} style={{ cursor: 'pointer' }} onClick={isLoggedIn ? () => { addServiceCollection(posts.did) } : handleShow} />}
                             <div style={{ position: "absolute", right: "30px", bottom: "0px" }}>更新時間：{posts.updated_at}</div>
                         </div>
                     </Col>
@@ -206,9 +256,9 @@ console.log(service_star_avg)
                             <div>{members.name}</div>
                             <div >
                                 評價：
-                                {service_star_avg !== 0 
-                                ? Array.from({ length: service_star_avg }, (_, i) => (<FaStar key={i} className="" style={{ color: "gold" }} />))
-                                : <>尚無評價</>}
+                                {service_star_avg !== 0
+                                    ? Array.from({ length: service_star_avg }, (_, i) => (<FaStar key={i} className="" style={{ color: "gold" }} />))
+                                    : <>尚無評價</>}
                             </div>
                             <div style={{ marginTop: "5px" }}>最後上線時間：{members.last_login}</div>
                             <div className="mt-3">
@@ -276,11 +326,42 @@ console.log(service_star_avg)
                     </Modal.Footer>
                 </Modal>
                 {/* 我要報價頁面 */}
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
+
+
+
+
+
+                {/* 類似案件 */}
+                <h4 className="mt-5">相同類別案件：</h4>
+                <div className="row " >
+                    <div className=" mb-4 container" style={{ display: "flex", justifyContent: "center" }}>
+                        <div style={{ display: "flex" }}>
+                            {othercase.slice(0, 3).map((post, index) => {
+                                return (
+                                    <Row key={index} style={{ margin: "30px 32px 30px 32px" }}>
+                                        <Col>
+                                            <Link to={`./casecontext/?${post.did}`} style={{ width: "30%", textDecoration: "none" }} >
+                                                <div className="toast show ">
+                                                    <div className="toast-header">
+                                                        <strong className="me-auto">
+                                                            <div>{post.d_name}</div>
+                                                            <div>預算：{post.d_amount}</div>
+                                                        </strong>
+                                                    </div>
+                                                    <div className="toast-body">
+                                                        <div>刊登日期：{post.created_at}</div>
+                                                        <div>地點：{post.country_city}</div>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </Col>
+                                    </Row>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+                {/* 類似案件 */}
 
             </div>
             <Footer></Footer>
