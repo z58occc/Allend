f2e721a605a07d691c6f6705d2f65383db9b47f7
sdfghjka <kxnenao100@yahoo.com.tw>
2024-04-18 15:08:33 +0800

    進行中發案紀錄

--- frontend/src/release/CaseDetailsModal2.js
+++ frontend/src/release/CaseDetailsModal2.js
@@ -92,19 +92,19 @@ const CaseDetailsModal2 = ({ show, onHide, number, data }) => {
               className="col"
               style={{ marginBottom: "10px", fontSize: "20px" }}
             >
-              <strong>接案人姓名：{data[number].c_contact_name}</strong>
+              <strong>接案人姓名：{data[number].name}</strong>
             </div>
             <div
               className="col"
               style={{ marginBottom: "10px", fontSize: "20px" }}
             >
-              <strong>接案人 Email：{data[number].c_email}</strong>
+              <strong>接案人 Email：{data[number].email}</strong>
             </div>
             <div
               className="col"
               style={{ marginBottom: "10px", fontSize: "20px" }}
             >
-              <strong>接案人手機：{data[number].c_mobile_phone}</strong>
+              <strong>接案人手機：{data[number].mobile_phone}</strong>
             </div>
           </div>
         </div>
