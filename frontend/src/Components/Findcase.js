2ff1d77d298c0778fde7677364debae42697dc71
Lee <a0981583030@gmail.com>
2024-06-19 14:57:43 +0800

    update

--- frontend/src/Components/Findcase.js
+++ frontend/src/Components/Findcase.js
@@ -382,33 +382,159 @@ function Findcase() {
   const [duration, setDuration] = useState("");
 
 
-  useEffect(() => {
-    switch (type) {
-      case "1":
-        newArray = findcaseData.filter((item) => item.type == "網站設計")
-        break;
-      case "2":
-        newArray = findcaseData.filter((item) => item.type == "軟體程式")
-        break;
-      case "3":
-        newArray = findcaseData.filter((item) => item.type == "平面設計")
-        break;
-      case "4":
-        newArray = findcaseData.filter((item) => item.type == "文字語言")
-        break;
-      case "5":
-        newArray = findcaseData.filter((item) => item.type == "專業諮詢")
-        break;
+  // useEffect(() => {
+  //   switch (type) {
+  //     case "1":
+  //       newArray = findcaseData.filter((item) => item.type == "網站設計")
+  //       break;
+  //     case "2":
+  //       newArray = findcaseData.filter((item) => item.type == "軟體程式")
+  //       break;
+  //     case "3":
+  //       newArray = findcaseData.filter((item) => item.type == "平面設計")
+  //       break;
+  //     case "4":
+  //       newArray = findcaseData.filter((item) => item.type == "文字語言")
+  //       break;
+  //     case "5":
+  //       newArray = findcaseData.filter((item) => item.type == "專業諮詢")
+  //       break;
 
-      default:
-        newArray = findcaseData
-        break;
-    }
-    setPosts(newArray);
-  }, [type])
+  //     default:
+  //       newArray = findcaseData
+  //       break;
+  //   }
+  //   setPosts(newArray);
+  // }, [type])
 
-  let secondArray;
+  // useEffect(() => {
+  //   const countryQuery = Object.keys(checkedState)
+  //     .filter((key) => checkedState[key])
+  //     .map((key) => {
+  //       switch (key) {
+  //         case "台北市":
+  //           return "台北市"
+  //         case "新北市":
+  //           return "新北市"
+  //         case "桃園市":
+  //           return "桃園市"
+  //         case "基隆市":
+  //           return "基隆市"
+  //         case "新竹市":
+  //           return "新竹市"
+  //         case "新竹縣":
+  //           return "新竹縣"
+  //         case "彰化縣":
+  //           return "彰化縣"
+  //         case "南投縣":
+  //           return "南投縣"
+  //         case "雲林縣":
+  //           return "雲林縣"
+  //         case "高雄市":
+  //           return "高雄市"
+  //         case "台南市":
+  //           return "台南市"
+  //         case "嘉義市":
+  //           return "嘉義市"
+  //         case "嘉義縣":
+  //           return "嘉義縣"
+  //         case "屏東縣":
+  //           return "屏東縣"
+  //         case "宜蘭縣":
+  //           return "宜蘭縣"
+  //         case "花蓮縣":
+  //           return "花蓮縣"
+  //         case "臺東縣":
+  //           return "臺東縣"
+  //         case "澎湖縣":
+  //           return "澎湖縣"
+  //         case "金門縣":
+  //           return "金門縣"
+  //         case "連江縣":
+  //           return "連江縣"
+  //         default:
+  //           return "";
+  //       }
+  //     })
+  //     .join(",");
+
+
+  //   if (countryQuery == false) {
+  //     setPosts(findcaseData);
+  //   } else {
+
+  //     setPosts((preData) => {
+  //       return (
+  //         preData.filter(item => countryQuery.includes(item.country_city))
+  //       )
+  //     })
+
+  //   }
+  // }, [checkedState])
+
+  // useEffect(() => {
+
+  //   const budgetQuery = Object.keys(budgetstate)
+  //     .filter((key) => budgetstate[key])
+  //     .map((key) => {
+  //       switch (key) {
+  //         case "五千":
+  //           return {
+  //             min: 0,
+  //             max: 5000
+  //           };
+  //         case "一萬":
+  //           return {
+  //             min: 5001,
+  //             max: 10000
+  //           };
+  //         case "五萬":
+  //           return {
+  //             min: 10001,
+  //             max: 50000
+  //           };
+  //         case "十萬":
+  //           return {
+  //             min: 50001,
+  //             max: 100000
+  //           };
+  //         case "三十萬":
+  //           return {
+  //             min: 100001,
+  //             max: 300000
+  //           };
+  //         default:
+  //           return null;
+  //       }
+  //     })
+
+  //   console.log(budgetQuery);
+
+  //   if (budgetQuery == false) {
+  //     setPosts(findcaseData)
+  //   } else {
+
+
+  //     setPosts((preData) => {
+  //       return (
+  //         preData.filter(item => {
+  //           return budgetQuery.some(range => item.d_amount >= range.min && item.d_amount <= range.max)
+  //         })
+  //       )
+  //     })
+
+  //   }
+
+
+
+
+  // }, [budgetstate])
+
   useEffect(() => {
+
+
+
+    //都市
     const countryQuery = Object.keys(checkedState)
       .filter((key) => checkedState[key])
       .map((key) => {
@@ -460,17 +586,10 @@ function Findcase() {
       .join(",");
 
 
-    if (countryQuery == false) {
-      setPosts(findcaseData);
-    } else {
-      secondArray = findcaseData.filter(item => countryQuery.includes(item.country_city));
-      setPosts(secondArray);
-    }
-  }, [checkedState])
 
-  let thirdArray;
-  useEffect(() => {
+    //都市
 
+    //預算
     const budgetQuery = Object.keys(budgetstate)
       .filter((key) => budgetstate[key])
       .map((key) => {
@@ -506,23 +625,80 @@ function Findcase() {
       })
 
     console.log(budgetQuery);
-    
-      if (budgetQuery == false) {
-        setPosts(findcaseData)
+
+
+    //預算
+
+    function fetchData({ type = "", city = [], budget = [] }) {
+      console.log(city);
+      //類別
+      switch (type) {
+        case "1":
+          newArray = findcaseData.filter((item) => item.type == "網站設計")
+          break;
+        case "2":
+          newArray = findcaseData.filter((item) => item.type == "軟體程式")
+          break;
+        case "3":
+          newArray = findcaseData.filter((item) => item.type == "平面設計")
+          break;
+        case "4":
+          newArray = findcaseData.filter((item) => item.type == "文字語言")
+          break;
+        case "5":
+          newArray = findcaseData.filter((item) => item.type == "專業諮詢")
+          break;
+
+        default:
+          newArray = findcaseData
+          break;
+      }
+      setPosts(newArray);
+
+      //類別
+
+      //都市
+      if (city == false) {
+        setPosts(findcaseData);
       } else {
-        thirdArray = findcaseData.filter(item => {
-          return budgetQuery.some(range => item.d_amount >= range.min && item.d_amount <= range.max)
+
+        setPosts((preData) => {
+          return (
+            preData.filter(item => city.includes(item.country_city))
+          )
         })
 
-        setPosts(thirdArray); 
       }
-    
+      //都市
+      // 預算
+      // if (budget == false) {
+      //   setPosts(findcaseData)
+      // } else {
+
+
+      //   setPosts((preData) => {
+      //     return (
+      //       preData.filter(item => {
+      //         return budget.some(range => item.d_amount >= range.min && item.d_amount <= range.max)
+      //       })
+      //     )
+      //   })
+
+      //   // 預算
+      // }
 
 
 
-  }, [budgetstate])
 
-  // 
+
+
+    }
+    fetchData({ type: type, city: countryQuery, budget: budgetQuery });
+
+  }
+
+    , [type, checkedState, budgetstate])
+
 
 
 
@@ -533,6 +709,9 @@ function Findcase() {
 
 
   // 分頁
+
+
+
   const lastPostIndex = currentPage * postsPerPage;
 
 
--- frontend/src/Components/fetchCaseData.js
+++ frontend/src/Components/fetchCaseData.js
@@ -1,83 +1,152 @@
-import { findcaseData } from "./findcaseData";
-function fetchData({ type = "", duration = "", city = "", budget = "" }) {
+// import { findcaseData } from "./findcaseData";
+// function fetchData({ type = "", duration = "", city = "", budget = "" }) {
+//     //類別
+//     switch (type) {
+//         case "1":
+//             newArray = findcaseData.filter((item) => item.type == "網站設計")
+//             break;
+//         case "2":
+//             newArray = findcaseData.filter((item) => item.type == "軟體程式")
+//             break;
+//         case "3":
+//             newArray = findcaseData.filter((item) => item.type == "平面設計")
+//             break;
+//         case "4":
+//             newArray = findcaseData.filter((item) => item.type == "文字語言")
+//             break;
+//         case "5":
+//             newArray = findcaseData.filter((item) => item.type == "專業諮詢")
+//             break;
 
-    // 分類type//
-    const newArray = [];
-    console.log(type);
-    switch (type) {
-        case "1":
-            for (let i = 0; i < findcaseData.length; i++) {
-                if (findcaseData[i].type == "網站設計") {
-                    newArray.push(findcaseData[i]);
-                }
-            }
-            break;
-        case "2":
-            for (let i = 0; i < findcaseData.length; i++) {
-                if (findcaseData[i].type == "軟體程式") {
-                    newArray.push(findcaseData[i]);
-                }
-            }
-            break;
-        case "3":
-            for (let i = 0; i < findcaseData.length; i++) {
-                if (findcaseData[i].type == "平面設計") {
-                    newArray.push(findcaseData[i]);
-                }
-            }
-            break;
-        case "4":
-            for (let i = 0; i < findcaseData.length; i++) {
-                if (findcaseData[i].type == "文字語言") {
-                    newArray.push(findcaseData[i]);
-                }
-            }
-            break;
-        case "5":
-            for (let i = 0; i < findcaseData.length; i++) {
-                if (findcaseData[i].type == "專業諮詢") {
-                    newArray.push(findcaseData[i]);
-                }
-            }
-            break;
+//         default:
+//             newArray = findcaseData
+//             break;
+//     }
+//     setPosts(newArray);
+
+//     //類別
 
 
-        default:
-            
-            break;
-    }
-    // 分類type//
+//     //都市
+//     const countryQuery = Object.keys(checkedState)
+//         .filter((key) => checkedState[key])
+//         .map((key) => {
+//             switch (key) {
+//                 case "台北市":
+//                     return "台北市"
+//                 case "新北市":
+//                     return "新北市"
+//                 case "桃園市":
+//                     return "桃園市"
+//                 case "基隆市":
+//                     return "基隆市"
+//                 case "新竹市":
+//                     return "新竹市"
+//                 case "新竹縣":
+//                     return "新竹縣"
+//                 case "彰化縣":
+//                     return "彰化縣"
+//                 case "南投縣":
+//                     return "南投縣"
+//                 case "雲林縣":
+//                     return "雲林縣"
+//                 case "高雄市":
+//                     return "高雄市"
+//                 case "台南市":
+//                     return "台南市"
+//                 case "嘉義市":
+//                     return "嘉義市"
+//                 case "嘉義縣":
+//                     return "嘉義縣"
+//                 case "屏東縣":
+//                     return "屏東縣"
+//                 case "宜蘭縣":
+//                     return "宜蘭縣"
+//                 case "花蓮縣":
+//                     return "花蓮縣"
+//                 case "臺東縣":
+//                     return "臺東縣"
+//                 case "澎湖縣":
+//                     return "澎湖縣"
+//                 case "金門縣":
+//                     return "金門縣"
+//                 case "連江縣":
+//                     return "連江縣"
+//                 default:
+//                     return "";
+//             }
+//         })
+//         .join(",");
 
-    // 分類duration
-    const secondArray=[];
-    switch (duration) {
-        case "短":
-            for (let i = 0; i < newArray.length; i++) {
-                if (newArray[i].d_duration == "短") {
-                    secondArray.push(newArray[i])
-                }
-            }
-            break;
-        case "長":
-            for (let i = 0; i < newArray.length; i++) {
-                if (newArray[i].d_duration == "長") {
-                    secondArray.push(newArray[i])
-                }
-            }
-            break;
+
+//     if (countryQuery == false) {
+//         setPosts(findcaseData);
+//     } else {
+
+//         setPosts((preData) => {
+//             return (
+//                 preData.filter(item => countryQuery.includes(item.country_city))
+//             )
+//         })
+
+//     }
+//     //都市
+
+//     //預算
+//     const budgetQuery = Object.keys(budgetstate)
+//         .filter((key) => budgetstate[key])
+//         .map((key) => {
+//             switch (key) {
+//                 case "五千":
+//                     return {
+//                         min: 0,
+//                         max: 5000
+//                     };
+//                 case "一萬":
+//                     return {
+//                         min: 5001,
+//                         max: 10000
+//                     };
+//                 case "五萬":
+//                     return {
+//                         min: 10001,
+//                         max: 50000
+//                     };
+//                 case "十萬":
+//                     return {
+//                         min: 50001,
+//                         max: 100000
+//                     };
+//                 case "三十萬":
+//                     return {
+//                         min: 100001,
+//                         max: 300000
+//                     };
+//                 default:
+//                     return null;
+//             }
+//         })
+
+//     console.log(budgetQuery);
+
+//     if (budgetQuery == false) {
+//         setPosts(findcaseData)
+//     } else {
 
 
-        default:
-            break;
-    }
-    // 分類duration
+//         setPosts((preData) => {
+//             return (
+//                 preData.filter(item => {
+//                     return budgetQuery.some(range => item.d_amount >= range.min && item.d_amount <= range.max)
+//                 })
+//             )
+//         })
 
-    // const cityFilter = findcaseData.filter(item=>city.includes(item.country_city));
-    // console.log(cityFilter);
-    // return cityFilter;
-    return newArray;
-}
-export default fetchData;
+//     }
+//     //預算
+
+// }
+// export default fetchData;
 // const response = await axios.get(
 //     `http://localhost/Allend/backend/public/api/findcase?type=${type}&location=${countryQuery}&amount=${budgetQuery}&d_duration=${durationQuery}&order=${orderQuery}`
 //   );
